697bff491c0a5d88b13500726b0253467414cb39
Albert Hambardzumyan <hambardzumyan.albert@gmail.com>
2018-04-13 19:03:03 +0400

    examples: add example for getTemplateColorPresets() of templates API.

---
+++ examples/templates/get-template-color-presets.js
@@ -0,0 +1,16 @@
+/**
+ * Copyright (c) 2018-present, Renderforest, LLC.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory.
+ */
+
+const Renderforest = require('../../src/lib/renderforest')
+
+const payload = {
+  templateId: 701
+}
+Renderforest.getTemplateColorPresets(payload)
+  .then(console.log) // handle the success
+  .catch(console.error) // handle the error
