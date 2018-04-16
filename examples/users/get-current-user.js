22507901accebea4cc246b217e7ddf88569f0126
Albert Hambardzumyan <hambardzumyan.albert@gmail.com>
2018-04-16 18:28:36 +0400

    examples: add example for getCurrentUser() of users API.

---
+++ examples/users/get-current-user.js
@@ -0,0 +1,16 @@
+/**
+ * Copyright (c) 2018-present, Renderforest, LLC.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory.
+ */
+
+const Renderforest = require('../../src/lib/renderforest')
+
+const options = { signKey: 'mock-signKey', clientId: -1 }
+const renderforest = new Renderforest(options)
+
+renderforest.getCurrentUser()
+  .then(console.log) // handle the success
+  .catch(console.error) // handle the error
