8c969e96ac95cccd2be3aab86bf6c2374a569a78
Albert Hambardzumyan <hambardzumyan.albert@gmail.com>
2018-05-25 16:39:30 +0400

    examples: add example for renderProject() of projects API.

---
+++ examples/projects/render-project.js
@@ -0,0 +1,19 @@
+/**
+ * Copyright (c) 2018-present, Renderforest, LLC.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory.
+ */
+
+const Renderforest = require('../../src/lib/renderforest')
+
+const renderforest = new Renderforest({ signKey: '<signKey>', clientId: -1 })
+
+const payload = {
+  projectId: 7015342,
+  quality: 0
+}
+renderforest.renderProject(payload)
+  .then(console.log) // handle the success
+  .catch(console.error) // handle the error
