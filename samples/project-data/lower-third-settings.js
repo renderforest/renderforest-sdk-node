51e5b849efe412a12dd38e82de94afe8072e89ae
narekhovhannisyan <narekgilmour@gmail.com>
2019-03-13 19:46:16 +0400

    samples: add lower third settings usage sample.
     - fixes #162

---
+++ samples/project-data/lower-third-settings.js
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) 2018-present, Renderforest, LLC.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory.
+ */
+
+const RenderforestClient = require('../../lib/client')
+
+const renderforest = new RenderforestClient({ signKey: '<signKey>', clientId: -1 })
+
+const payload = {
+  projectId: 15220886
+}
+
+renderforest.getProjectData(payload)
+  .then((projectDataInstance) => {
+    const screen = projectDataInstance.getScreen(0)
+
+    return screen
+      .setLowerThirdSettings(2, 5)
+      .getPayloadData()
+  })
+  .then(renderforest.updateProjectDataPartial)
+  .catch(console.error)
