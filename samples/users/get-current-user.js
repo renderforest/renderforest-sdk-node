7ffcc526a8749607abe18f07fb29764c0b39c6b4
narekhovhannisyan <narekgilmour@gmail.com>
2019-03-18 19:58:09 +0400

    samples: capitalize class usage in users.

--- samples/users/get-current-user.js
+++ samples/users/get-current-user.js
@@ -8,8 +8,8 @@
 
 const RenderforestClient = require('../../lib/client')
 
-const renderforest = new RenderforestClient({ signKey: '<signKey>', clientId: -1 })
+const Renderforest = new RenderforestClient({ signKey: '<signKey>', clientId: -1 })
 
-renderforest.getCurrentUser()
+Renderforest.getCurrentUser()
   .then(console.log) // handle the success
   .catch(console.error) // handle the error
