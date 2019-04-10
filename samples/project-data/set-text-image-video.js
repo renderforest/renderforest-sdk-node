e201a55ffc3d02c257f70e0ef84eb91e5d5866e5
narekhovhannisyan <narekgilmour@gmail.com>
2019-04-10 23:43:12 +0400

    samples: update the example of setting text, image, video.

--- samples/project-data/set-text-image-video.js
+++ samples/project-data/set-text-image-video.js
@@ -10,52 +10,53 @@ const RenderforestClient = require('../../lib/client')
 
 const Renderforest = new RenderforestClient({ signKey: '<signKey>', clientId: -1 })
 
-Renderforest.getProjectData(15220886)
+Renderforest.getProjectData(16165971)
   .then((projectDataInstance) => {
-    const firstScreen = projectDataInstance.getScreen(0)
-    const firstAreaFirstScreen = firstScreen.getArea(0)
-    console.log(firstAreaFirstScreen.getRecommendedCharacterCount())
-    firstAreaFirstScreen.setText('sample-text')
+    // check area type
+    console.log(projectDataInstance.getScreen(0).getArea(0).getAreaType())
+    projectDataInstance.getScreen(0)
+      .getArea(0)
+      .setText('sample-text')
+      .setTextScale(120)
 
-    const secondScreen = projectDataInstance.getScreen(1)
-    const firstAreaSecondScreen = secondScreen.getArea(0)
-    console.log(firstAreaSecondScreen.getAreaType())
-    firstAreaSecondScreen.setImage({
-      fileName: 'sample file name', // optional
-      mime: 'image/png', // optional
-      filePath: 'https://example.com/sample.png',
-      webpPath: 'https://example.com/sample.webp', // optional
-      fileType: 'image', // optional
-      thumbnailPath: 'https://example.com/sample-thumbnail.png', // optional
-      imageCropParams: {
-        transform: 0,
-        top: 11,
-        left: 0,
-        width: 798,
-        height: 456
-      }
-    })
-
-    const thirdScreen = projectDataInstance.getScreen(2)
-    const firstAreaThirdScreen = thirdScreen.getArea(0)
-    firstAreaThirdScreen.setVideo({
-      fileName: 'sample file name', // optional
-      mime: 'video/mp4', // optional
-      filePath: 'https://example.com/sample.png',
-      webpPath: 'https://example.com/sample.webp', // optional
-      fileType: 'video', // optional
-      videoCropParams: {
-        duration: 6,
-        mime: 'video/mp4',
-        thumbnail: 'https://example.com/sample-thumbnail.png',
-        thumbnailVideo: 'https://example.com/sample-thumbnail-video.mp4',
-        trims: [0, 2, 3, 5],
-        volume: {
-          music: 10,
-          video: 100
+    projectDataInstance.getScreen(1)
+      .getArea(0)
+      .setImage({
+        fileName: 'sample file name', // optional
+        mime: 'image/png', // optional
+        filePath: 'https://example.com/sample.png',
+        webpPath: 'https://example.com/sample.webp', // optional
+        fileType: 'image', // optional
+        thumbnailPath: 'https://example.com/sample-thumbnail.png', // optional
+        imageCropParams: {
+          transform: 0,
+          top: 11,
+          left: 0,
+          width: 798,
+          height: 456
         }
-      }
-    })
+      })
+
+    projectDataInstance.getScreen(2)
+      .getArea(0)
+      .setVideo({
+        fileName: 'sample file name', // optional
+        mime: 'video/mp4', // optional
+        filePath: 'https://example.com/sample.png',
+        webpPath: 'https://example.com/sample.webp', // optional
+        fileType: 'video', // optional
+        videoCropParams: {
+          duration: 6,
+          mime: 'video/mp4',
+          thumbnail: 'https://example.com/sample-thumbnail.png',
+          thumbnailVideo: 'https://example.com/sample-thumbnail-video.mp4',
+          trims: [0, 2, 3, 5],
+          volume: {
+            music: 10,
+            video: 100
+          }
+        }
+      })
 
     return projectDataInstance.save()
   })
