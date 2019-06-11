22006abefd53ec8a95076bc61e3972daaec6166b
narekhovhannisyan <narekgilmour@gmail.com>
2019-06-11 15:05:11 +0400

    samples: in project data lower third settings use new pattern.

--- samples/project-data/lower-third-settings.js
+++ samples/project-data/lower-third-settings.js
@@ -11,12 +11,10 @@ const RenderforestClient = require('../../lib/client')
 const Renderforest = new RenderforestClient({ signKey: '<signKey>', clientId: -1 })
 
 Renderforest.getProjectData(15220886)
-  .then((projectDataInstance) => {
-    const screen = projectDataInstance.getScreen(0)
-
-    screen
-      .setLowerThirdSettings(2, 5)
-
-    return projectDataInstance.save()
-  })
+  .then((projectDataInstance) =>
+    projectDataInstance.getScreen(0)
+      .setLowerThirdSettings(2, 6)
+      .save()
+  )
+  .then(console.log)
   .catch(console.error)
